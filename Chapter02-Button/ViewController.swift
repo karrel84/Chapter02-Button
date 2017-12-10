51d95e437f40ec9b62f0f542198cc3714bd9aaea
karrel84 <karrel84@naver.com>
2017-12-10 23:34:07 +0900

    버튼 인스턴스를 생성하ㅗㄱ, 속성을 설정 버튼을 수평 중앙 정렬 루트 뷰에 버튼을 추가

--- Chapter02-Button/ViewController.swift
+++ Chapter02-Button/ViewController.swift
@@ -12,7 +12,19 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // 버튼 인스턴스를 생성하고, 속성을 설정한다.
+        let btn = UIButton(type: UIButtonType.system) // 1
+        btn.frame = CGRect(x: 50, y: 100, width: 150, height: 30) // 2
+        btn.setTitle("테스트 버튼", for:UIControlState.normal) //3
+        
+        // 버튼을 수평 중앙 정렬한다.
+        btn.center = CGPoint(x: self.view.frame.size.width / 2, y: 100)
+        
+        
+        // 루트 뷰에 버튼을 추가한다.
+        self.view.addSubview(btn)
+        
     }
 
     override func didReceiveMemoryWarning() {
